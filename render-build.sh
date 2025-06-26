7f20426d1c8d8f11597c11ccf1b0d0818c3ef0dc
3bassz <ahmedmabbas46@gmail.com>
2025-06-26 07:21:40 +0300

    🐛 Fix: Add npm install to render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
 echo "✅ Installing Chrome for Puppeteer"
 npx puppeteer browsers install chrome
+
+echo "📦 Installing Node dependencies"
+npm install
+
+echo "🚀 Starting the app"
+npm start
